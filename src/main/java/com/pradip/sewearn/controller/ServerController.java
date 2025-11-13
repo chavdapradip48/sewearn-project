6790487bf388de30ea70ddb29457708a9796b73a
Pradip Chavda <pradip@inexture.com>
2025-11-14 02:25:50 +0530

    redirection

--- src/main/java/com/pradip/sewearn/controller/ServerController.java
+++ src/main/java/com/pradip/sewearn/controller/ServerController.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @CrossOrigin(origins = "*")
 public class ServerController {
-    @GetMapping("/")
-    public String getIndex(){
-        return "Server is running......";
-    }
-
     @GetMapping("/keep-alive")
     @ResponseBody
     public String getKeepAlive(){
         return "Server is running......";
+    }
+    @GetMapping("/")
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui/index.html";
     }
 }
