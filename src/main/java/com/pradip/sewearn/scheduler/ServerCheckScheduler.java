430f9002f3e0508d4a007ca3b797832564932939
“Chavda <chavdasandip2908@gmail.com>
2025-11-15 17:33:24 +0530

    Scheduler changes for the log

--- src/main/java/com/pradip/sewearn/scheduler/ServerCheckScheduler.java
+++ src/main/java/com/pradip/sewearn/scheduler/ServerCheckScheduler.java
@@ -19,8 +19,8 @@ public class ServerCheckScheduler {
     public void callApi() {
         try{
             String response = restTemplate.getForObject(apiUrl+"/sewearn/keep-alive", String.class);
-//            System.out.println("\n=> Response from server : "+response+".");
-            String[] timeDate = LocalDateTime.now().toString().split("T");
+            System.out.println("\n=> Response from server : "+response+".");
+//            String[] timeDate = LocalDateTime.now().toString().split("T");
 //            System.out.println("=> Current Time         : "+timeDate[1].substring(0,timeDate[1].indexOf("."))+"");
 //            System.out.println("=> Current Date         : "+timeDate[0]+"\n");
         } catch (Exception ex){
